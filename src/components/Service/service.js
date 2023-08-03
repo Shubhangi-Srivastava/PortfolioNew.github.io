6e9c4388e8e929ad576e13d42548ff5da8f5ddf4
Shubhangi-Srivastava <srivastava.sh@northeastern.edu>
2023-08-03 16:29:56 -0400

    added education

--- src/components/Service/service.js
+++ src/components/Service/service.js
@@ -1,54 +1,85 @@
-
-
 /*--------------------
 * Service Section
 ----------------------*/
 function Service(props) {
-    return (
-        <>
-            <div className="lg:col-span-6">
-                <div className={props.id.main + " feature-box-01"}>
-                    <div className="icon"><i className={props.id.icon}></i></div>
-                    <div className="feature-content">
-                        <h5>{props.title}</h5>
-                        <p>{props.description}</p>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="lg:col-span-6">
+        <div className={props.id.main + " feature-box-01"}>
+          <div className="icon">
+            <img src={props.id.icon}></img>
+            {/* <img className={props.id.icon}></img> */}
+          </div>
+          <div className="feature-content">
+            <h5>{props.title}</h5>
+            <h6>{props.subtitle}</h6>
+            <h6>{props.graduation}</h6>
+            <p>{props.description}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 /*--------------------
 * Service List Section
 ----------------------*/
 export default function ServiceList() {
-    const service_list = [
-        {id:{main:'bg-1', icon:'fas fa-phone-alt'}, title:'Web Design', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
-        {id:{main:'bg-2', icon:'fas fa-laptop'}, title:'Development', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
-        {id:{main:'bg-3', icon:'fas fa-exclamation-triangle'}, title:'SEO Marketing', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
-        {id:{main:'bg-4', icon:'fas fa-columns'}, title:'Web Design', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
-        {id:{main:'bg-5', icon:'fas fa-ruler-vertical'}, title:'Development', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
-        {id:{main:'bg-6', icon:'fas fa-globe'}, title:'SEO Marketing', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
-    ]
-    return (
-        <>
-           <section data-scroll-data="1" id="services" className="section services-section bg-gray" style={{ "backgroundImage": "url(img/effect/bg-effect-1.png)", "backgroundRepeat": "no-repeat", "backgroundSize": "cover"}}>
-                <div className="container">
-                    <div className="grid section-heading">
-                        <div className="lg:col-span-6 text-center mx-auto">
-                            <h3><span>My Services</span></h3>
-                        </div>
-                    </div>
-                    <div className="grid lg:grid-cols-12 md:grid-cols-2 gap-5 grid-cols-1 items-center">
-                        {
-                            service_list.map((val, i)=>{
-                                return <Service key={i} id={val.id} title={val.title} description={val.description} />
-                            })
-                        }
-                    </div>
-                </div>
-            </section>
-        </>
-    );
+  const service_list = [
+    {
+      id: { main: "bg-1", icon: "../../../public/img/neuLogo.jpeg" },
+      title: "Northeastern University, College of Engineering",
+      subtitle: <i>Masters in Computer Software Engineering</i>,
+      graduation: "Expected Graduation: December 2023",
+      description:
+        "Related Courses: Application Engineering and Development, Database Management and database Design, Object Oriented Design and Principles, Web Design / User Experience Engineering, UIUX Design and Testing, Agile Development and Methodologies, Managerial Communications",
+    },
+    {
+      id: { main: "bg-2", icon: "fas fa-laptop" },
+      title: "Pune University, Maharashtra Institute of Technology",
+      subtitle: <i>Bachelors in Computer Sciences</i>,
+      graduation: "Graduation: July 2019",
+      description:
+        "Related Courses: Concepts of OOPs, Database Management, Web Design, Data Structures and Algorithms",
+    },
+  ];
+  return (
+    <>
+      <section
+        data-scroll-data="1"
+        id="services"
+        className="section services-section bg-gray"
+        style={{
+          backgroundImage: "url(img/effect/bg-effect-1.png)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="container">
+          <div className="grid section-heading">
+            <div className="lg:col-span-6 text-center mx-auto">
+              <h3>
+                <span>My Education</span>
+              </h3>
+            </div>
+          </div>
+          <div className="grid lg:grid-cols-1 md:grid-cols-1 gap-5 grid-cols-1 items-center">
+            {service_list.map((val, i) => {
+              return (
+                <Service
+                  key={i}
+                  id={val.id}
+                  title={val.title}
+                  subtitle={val.subtitle}
+                  graduation={val.graduation}
+                  description={val.description}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
