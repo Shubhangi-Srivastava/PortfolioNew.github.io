b13f9832b91165ab6f0d777989828347a0bf2bc6
Shubhangi-Srivastava <srivastava.sh@northeastern.edu>
2023-08-04 16:29:02 -0400

    added some new styling for Skills section and fixed the grid: needs more fixing

--- src/components/Skills/skill.js
+++ src/components/Skills/skill.js
@@ -23,25 +23,30 @@ export default function SkillList() {
   const skill_list = [
     { id: { main: "bg-1", icon: "fab fa-java" }, title: "Java" },
     { id: { main: "bg-2", icon: "fab fa-js" }, title: "Javascript" },
-    { id: { main: "bg-2", icon: "fab fa-html5" }, title: "HTML" },
-    { id: { main: "bg-2", icon: "fab fa-css3" }, title: "CSS" },
-    { id: { main: "bg-3", icon: "fab fa-react" }, title: "React JS" },
-    { id: { main: "bg-4", icon: "fa-solid fa-database" }, title: "MySQL" },
+    { id: { main: "bg-3", icon: "fab fa-html5" }, title: "HTML" },
+    { id: { main: "bg-4", icon: "fab fa-css3" }, title: "CSS" },
+    { id: { main: "bg-5", icon: "fab fa-react" }, title: "React JS" },
+    { id: { main: "bg-6", icon: "fab fa-data" }, title: "MySQL" },
+    { id: { main: "bg-1", icon: "fab fa-data" }, title: "RestAPI" },
+    { id: { main: "bg-2", icon: "fab fa-data" }, title: "Tailwind CSS" },
+    { id: { main: "bg-3", icon: "fab fa-data" }, title: "C#" },
+    { id: { main: "bg-4", icon: "fab fa-data" }, title: ".NET" },
   ];
 
   return (
     <>
       <section
-        data-scroll-data="2"
+        data-scroll-data="3"
         id="skill"
-        className="section experience-section !pb-0"
+        className="section skill-section !pb-0"
       >
         <div className="container">
-          <div className="grid lg:grid-cols-12 md:grid-cols-2 grid-cols-1">
+          <div className="">
+            {/*}grid lg:grid-cols-12 md:grid-cols-2 grid-cols-1 */}
             <div className="lg:col-span-6 lg:pl-9">
               <div className="section-heading">
                 <h3 className="m-0">
-                  <span>My Skills</span>
+                  <span>Skills</span>
                 </h3>
               </div>
               <div className="skill-box">
@@ -52,9 +57,9 @@ export default function SkillList() {
                 </div>
               </div>
             </div>
-            <div className="lg:col-span-6 text-center pt-[40px] md:pt-0">
+            {/* <div className="lg:col-span-6 text-center pt-[40px] md:pt-0">
               <img className="mx-[auto]" src="" title="" alt="" />
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
